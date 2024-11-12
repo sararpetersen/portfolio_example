d58994e139e638ae52519f69f94ba38d18d8dda4
Sara <sape0003@stud.kea.dk>
2024-11-12 23:01:13 +0100

    Main font-colour changed

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,20 +4,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        "site-black": "#000000",
-        "site-dark-gray": "#1e1e1e",
-        "site-gray": "#4a4a4a",
-        "site-light-gray": "#b3b3b3",
         "site-red": "#ff3333",
       },
 
       fontFamily: {
         almarai: ["Almarai", "sans-serif"],
         exo: ["Exo", "sans-serif"],
-      },
-
-      cursor: {
-        handcursor: "url(/cursor.png) 25 25, pointer",
       },
     },
   },
